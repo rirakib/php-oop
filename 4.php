25aa350e6ee8592b3aa0d3ea2be6c42c9df02935
rirakib <rirrakib10@gmail.com>
2023-09-16 11:34:23 -0700

    updated

--- 4.php
+++ 4.php
@@ -1,2 +1,27 @@
 <?php
-    
+
+    class Main{
+
+        function wish()
+        {
+            echo "hi from main class";
+        }
+
+        //can not override it in children class
+        final function bye()
+        {
+            echo "bye from main class";
+        }
+    }
+
+    class Drived extends Main{
+
+        //override method
+        function wish()
+        {
+            echo "hi from drived class";
+        }
+    }
+
+    $ob1 = new Drived();
+    $ob1->wish();
